f118492103675dc8d7728a2133a53eb54b004159
Saba Morchilashvili <smorchilashvili@gmail.com>
2024-01-27 07:20:37 +0400

    added loggers for debugging

--- demo/src/main/java/com/example/demo/player/Player.java
+++ demo/src/main/java/com/example/demo/player/Player.java
@@ -1,8 +1,11 @@
 package com.example.demo.player;
 
 import com.example.demo.team.Team;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.ToString;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
 
@@ -16,6 +19,8 @@ import org.springframework.lang.Nullable;
 // Seemingly the problem is bidirectional (@OneToMany) relationship between entities, but I'm not sure.
 public class Player {
 
+    private static final Logger logger = LogManager.getLogger(Player.class);
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -25,7 +30,7 @@ public class Player {
     private String lastName;
     @Transient
     private String fullName;
-    @Nullable
+//    @JsonIgnore
     @ManyToOne(optional = true)
     @JoinColumn(name = "team_id")
     private Team team;
@@ -33,28 +38,41 @@ public class Player {
     private Integer jerseyNumber;
 
     public Player() {
+        logger.info("Player created!");
     }
 
     public Player(String firstName, String lastName) {
-        if (firstName == null || lastName == null)
+        if (firstName == null || lastName == null) {
+            logger.error("Invalid firstname or lastname!");
             throw new IllegalArgumentException("Firstname and lastname can't be null!");
+        }
         this.firstName = firstName;
         this.lastName = lastName;
+        logger.info("Player created!");
     }
 
     public Player(String firstName, String lastName, Team team, Integer jerseyNumber) {
-        if (firstName == null || lastName == null)
+        if (firstName == null || lastName == null) {
+            logger.error("Invalid firstname or lastname!");
             throw new IllegalArgumentException("Firstname and lastname can't be null!");
-        if (team != null && jerseyNumber == null)
+        }
+        if (team != null && jerseyNumber == null) {
+            logger.error("Invalid jersey number!");
             throw new IllegalArgumentException("When in team, jersey number can't be null!");
-        if (team != null && jerseyNumber < 1)
+        }
+        if (team != null && jerseyNumber < 1) {
+            logger.error("Invalid jersey number!");
             throw new IllegalArgumentException("Player numbering begins from 1 upwards!");
-        else if (team == null && jerseyNumber != null)
+        }
+        else if (team == null && jerseyNumber != null) {
+            logger.error("Invalid team!");
             throw new IllegalArgumentException("If player has jersey number, he/she must be in a team!");
+        }
         this.firstName = firstName;
         this.lastName = lastName;
         this.team = team;
         this.jerseyNumber = jerseyNumber;
+        logger.info("Player created!");
     }
 
     public Long getId() {
@@ -82,7 +100,7 @@ public class Player {
     }
 
     public String getFullName() {
-        return firstName + lastName;
+        return firstName + " " + lastName;
     }
 
     public Team getTeam() {
@@ -90,13 +108,6 @@ public class Player {
     }
 
     public void setTeam(Team team) {
-        if (this.team == null) {
-            this.team = team;
-            return;
-        }
-        Team oldTeam = this.team;
-        // TODO: Properly remove the player from playersList of the old team
-        // TODO: Properly add the player to the new team
         this.team = team;
     }
 
--- demo/src/main/java/com/example/demo/team/Team.java
+++ demo/src/main/java/com/example/demo/team/Team.java
@@ -1,8 +1,12 @@
 package com.example.demo.team;
 
 import com.example.demo.player.Player;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.ToString;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.lang.NonNull;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,25 +16,40 @@ import java.util.List;
 //@ToString
 public class Team {
 
+    private static final Logger logger = LogManager.getLogger(Team.class);
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NonNull
     private String name;
+    @NonNull
     private String city;
-    @OneToMany(mappedBy = "team")
+    @JsonIgnore
+    @OneToMany(mappedBy = "team", cascade = CascadeType.ALL)
     private List<Player> players = new ArrayList<>();
     @Transient
     private int numberOfPlayers;
 
     public Team() {
+        logger.info("Team created!");
     }
 
     public Team(String name, String city) {
+        if (name == null || city == null) {
+            logger.error("Invalid name or city!");
+            throw new IllegalArgumentException("Name or city can't be null!");
+        }
         this.name = name;
         this.city = city;
+        logger.info("Team created!");
     }
 
     public Team(String name, String city, List<Player> players) {
+        if (name == null || city == null) {
+            logger.error("Invalid name or city!");
+            throw new IllegalArgumentException("Name or city can't be null!");
+        }
         this.name = name;
         this.city = city;
         this.players = players;
@@ -38,6 +57,10 @@ public class Team {
 
     public Long getId() {
         return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getName() {
